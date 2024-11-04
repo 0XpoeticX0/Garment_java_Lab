56acae743b35cf632c61df5a8c877141653fe947
0XpoeticX0 <ashfikur.rahaman2018@gmail.com>
2024-11-04 21:51:50 +0600

    first commit

---
+++ src/main/java/com/mycompany/garment/Garment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.garment;
+
+/**
+ *
+ * @author MaHir0
+ */
+public class Garment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
